4041106fb1990b8a19a7a8fea387e1acecc7b593
Christian Black <christian.black.cb@gmail.com>
2021-09-15 16:30:37 +0300

    Create lab_1.hs

---
+++ lab_1.hs
@@ -0,0 +1,18 @@
+{-
+
+Exercitii 
+1.  Cum adaugati 4 la sfarsitul listei [1,2,3] ? 
+2.  Gasiti  cel  putin  2  definitii  echivalente  pentru  functia  de  librarie last  (returneaza  ultimul element al unei liste nevide), folosind functiile explicate in acest laborator.
+3.  Gasiti cel putin 2 definitii echivalente pentru functia de librarie init(sterge ultimul element dintr-o lista nevida), folosind functiile explicate in acest laborator.
+4.  Scrieti o functie fib n care returneaza  al n-lea element din sirul lui fibonacci. Scrieti o functie fibonacci n care returneaza lista cu primele n elemente din sirul lui fibonacci (folosind, eventual, functia fib)
+
+-}
+
+-- 1.  Cum adaugati 4 la sfarsitul listei [1,2,3] ?
+reverse (4:reverse [1,2,3])
+
+-- 2.  Gasiti  cel  putin  2  definitii  echivalente  pentru  functia  de  librarie last  (returneaza  ultimul element al unei liste nevide), folosind functiile explicate in acest laborator.
+
+-- 3.  Gasiti cel putin 2 definitii echivalente pentru functia de librarie init(sterge ultimul element dintr-o lista nevida), folosind functiile explicate in acest laborator.
+
+-- 4.  Scrieti o functie fib n care returneaza  al n-lea element din sirul lui fibonacci. Scrieti o functie fibonacci n care returneaza lista cu primele n elemente din sirul lui fibonacci (folosind, eventual, functia fib)
