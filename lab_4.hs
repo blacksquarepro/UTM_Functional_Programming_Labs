19b0c6555054dcc029fe50501998f6e2cac83dc4
Christian <christian@blacksquare.pro>
2021-09-19 19:24:52 +0300

    Add lab4 conditions

---
+++ lab_4.hs
@@ -0,0 +1,14 @@
+{-
+
+1.  Se considera o functie safeTail::[a]->[a] ce se comporta similar cu functia de librarie tail, cu exceptia ca in cazul listei vide safeTail va returna aceeasi lista vida, pe cand tail genereaza eroare. Definiti safeTail folosind: 
+•        o expresie conditionala 
+•        “guarded ecuations” 
+•        “pattern matching” 
+
+2.  Scrieti o functie subList care verifica daca o lista e inclusa in alta. 
+
+3.  Definiti o functie pentru calculul produsului a 2 polinoame. Un polinom se va reprezenta prin lista coeficientilor. 
+
+4.  Scrieti o functie pascal :: Int -> [Int] , unde pascal n returneaza randul n din triunghiul lui Pascal.
+
+-}
